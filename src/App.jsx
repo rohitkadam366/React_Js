a0a67d3042622052ac2ae38c75cfdb2a9ba08156
Rohit Kadam <Rohit.Kadam@facileserv.local>
2024-07-26 14:31:05 +0530

    js

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import KgButton from "./KgButton"
+
+function App() {
+  return <div>
+    <h1>This is Best React Course</h1>
+    <KgButton></KgButton>
+  </div>
+}
+
+export default App;
---
+++ src/KgButton.jsx
@@ -0,0 +1,5 @@
+function KgButton() {
+  return <button>Like This Vedio</button>
+}
+
+export default KgButton;
